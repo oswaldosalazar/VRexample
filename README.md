09a9dc824e52c47e5b618a494001fa575838ed18
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-11 12:59:37 -0600

    Added index.html and main.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # VRexample
+
+
+This is an example of the use of the VR View library to display a 360˚ movie.
---
+++ main.js
@@ -0,0 +1,23 @@
+//Set the event target to a window, on load perform callback
+window.addEventListener('load', onVrViewLoad);
+
+function onVrViewLoad() {
+	//define vrView, assign it to id vrview and provide parameters
+  var vrView = new VRView.Player('#vrview', {
+    video: 'https://vr.jwplayer.com/content/AgqYcfAT/AgqYcfAT-8yQ1cYbs.mp4',
+    is_stereo: false,
+    width: window.innerWidth,
+		height: window.innerHeight
+  });
+	vrView.on('ready',function(){
+  	vrView.addHotspot('#hotspot', {
+    	pitch: 7, // In degrees. Up is positive.
+			yaw: 35.5, // In degrees. To the right is positive.
+			radius:   0.03, // Radius of the circular target in meters.
+			distance: 2, // Distance of target from camera in meters.
+    });
+	});
+};
+
+//Execute function
+onVrViewLoad();
