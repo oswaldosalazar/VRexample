6f0ecd9dc0453b26299b70acff1cbff3704d0451
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-12 12:30:14 -0600

    Fixed indentation in main.js

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 window.addEventListener('load', onVrViewLoad);
 
 function onVrViewLoad() {
-	//define vrView, assign it to id vrview and provide parameters
+  //define vrView, assign it to id vrview and provide parameters
   var vrView = new VRView.Player('#vrview', {
     video: 'https://vr.jwplayer.com/content/AgqYcfAT/AgqYcfAT-8yQ1cYbs.mp4',
     is_stereo: false,
     width: window.innerWidth,
-		height: window.innerHeight
+    height: window.innerHeight
   });
-	vrView.on('ready',function(){
-  	vrView.addHotspot('#hotspot', {
-    	pitch: 7, // In degrees. Up is positive.
-			yaw: 35.5, // In degrees. To the right is positive.
-			radius:   0.03, // Radius of the circular target in meters.
-			distance: 2, // Distance of target from camera in meters.
+  vrView.on('ready',function(){
+    vrView.addHotspot('#hotspot', {
+      pitch: 7, // In degrees. Up is positive.
+      yaw: 35.5, // In degrees. To the right is positive.
+      radius:   0.03, // Radius of the circular target in meters.
+      distance: 2, // Distance of target from camera in meters.
     });
-	});
+  });
 };
 
 //Execute function
